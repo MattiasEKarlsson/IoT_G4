060104c763f4a12054fe040fd5515e7b260267dc
Mattias Karlsson <Mattias.E.Karlsson@outlook.com>
2020-09-28 14:54:47 +0200

    NewDeviseID

--- Test_33/MainPage.xaml.cs
+++ Test_33/MainPage.xaml.cs
@@ -1,26 +1,14 @@
 ﻿using Microsoft.Azure.Devices.Client;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using Test_33.Services;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 
 
 namespace Test_33
 {
-   
-    
+
+
     public sealed partial class MainPage : Page
     {
         
@@ -28,7 +16,7 @@ namespace Test_33
         {
             this.InitializeComponent();
         }
-        private static readonly string _conn = "HostName=ec-win20iothub.azure-devices.net;DeviceId=consoleapp;SharedAccessKey=s5bq+AsW6yo+00GMDTgvNVWUUgNd+Mye35x/6wbktmo=";
+        private static readonly string _conn = "HostName=ec-win20iothub.azure-devices.net;DeviceId=DeviceApp;SharedAccessKey=sJGB59/d4EwPyNVxsX/VXWzxQoZkivpeYQUN+Bu7j+k=";
         private static readonly DeviceClient deviceClient = DeviceClient.CreateFromConnectionString(_conn, TransportType.Mqtt);
 
         private void btnSend_Click(object sender, RoutedEventArgs e)
